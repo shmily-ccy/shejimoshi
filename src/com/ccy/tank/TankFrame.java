776a10e21e6fe456fdfdeef525303bde4cfe7a46
ccy <18510247106@163.com>
2021-05-23 16:47:30 +0800

    将坦克坐标定为变量,在paint中改变值,paint被调用就可以动起来

--- src/com/ccy/tank/TankFrame.java
+++ src/com/ccy/tank/TankFrame.java
@@ -5,6 +5,9 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class TankFrame extends Frame {
+
+    int x=200,y=200;
+
     public TankFrame(){
         setSize(800,600);
         setResizable(false);
@@ -18,9 +21,17 @@ public class TankFrame extends Frame {
         });
    }
 
+    /**
+     * 将原来的背景全部清除一遍,然后再出现方块
+     * @param g
+     */
     @Override
     public void paint(Graphics g) {
-        g.fillRect(200,200,50,50);
+        //每次最小化窗口,就会进行移动,所以需要不断的调用这个方法
+        System.out.println("paint");
+        g.fillRect(x,y,50,50);
+        x+=10;
+        y+=10;
     }
 
 }
