32f555181f29b882d58720e06244b1650e511813
ccy <18510247106@163.com>
2021-05-23 14:34:03 +0800

    初步认识frame类,frame类继承,并且重写paint方法,画出一个黑方框

---
+++ src/com/ccy/tank/T.java
@@ -0,0 +1,23 @@
+package com.ccy.tank;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class T {
+    public static void main(String[] args) {
+        Frame f=new Frame();
+        f.setSize(800,600);
+        f.setResizable(false);
+        f.setVisible(true);
+        f.setTitle("tank war");
+        //添加window的监听器
+        f.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+}
---
+++ src/com/ccy/tank/TankFrame.java
@@ -0,0 +1,31 @@
+package com.ccy.tank;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class TankFrame extends Frame {
+    public TankFrame(){
+        setSize(800,600);
+        setResizable(false);
+        setTitle("tank war");
+        setVisible(true);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+   }
+
+    @Override
+    public void paint(Graphics g) {
+        g.fillRect(200,200,50,50);
+    }
+
+    public static void main(String[] args) {
+        TankFrame tf=new TankFrame();
+    }
+}
+
+
