f388ddf3e8ffd8fd436c55b00f07cb33843f6e3e
shmily-ccy <18510247106@163.com>
2021-05-23 21:58:29 +0800

    定义坦克方向的枚举类,根据按键状态确定坦克反向,根据坦克方向确定坦克位移

---
+++ src/com/ccy/tank/Dir.java
@@ -0,0 +1,5 @@
+package com.ccy.tank;
+
+public enum Dir {
+    LEFT,RIGHT,UP,DOWN
+}
--- src/com/ccy/tank/TankFrame.java
+++ src/com/ccy/tank/TankFrame.java
@@ -9,6 +9,8 @@ import java.awt.event.WindowEvent;
 public class TankFrame extends Frame {
 
     int x=200,y=200;
+    Dir dir=Dir.DOWN;
+    final int SPEED=10;
 
     public TankFrame(){
         setSize(800,600);
@@ -35,28 +37,89 @@ public class TankFrame extends Frame {
         //每次最小化窗口,就会进行移动,所以需要不断的调用这个方法(刷新)
         System.out.println("paint");
         g.fillRect(x,y,50,50);
-        x+=10;
-//        y+=10;
+        switch (dir){
+            case LEFT:
+                x-=SPEED;
+                break;
+            case RIGHT:
+                x+=SPEED;
+                break;
+            case UP:
+                y-=SPEED;
+                break;
+            case DOWN:
+                y+=SPEED;
+                break;
+            default:
+                break;
 
+        }
     }
 
     /**
      * 键盘的监听处理对象
      */
     class MyKeyListener extends KeyAdapter{
+        boolean bL=false;
+        boolean bU=false;
+        boolean bR=false;
+        boolean bD=false;
 
         @Override
         public void keyPressed(KeyEvent e) {
             //键按下的时候自动调用
 //            System.out.println("key press");
             //x+=200;
+            int keyCode = e.getKeyCode();
+            switch(keyCode){
+                case KeyEvent.VK_LEFT:
+                    bL=true;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU=true;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD=true;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR=true;
+                    break;
+                default:
+                    break;
 
+            }
+            setMainTankDir();
+        }
+
+        private void setMainTankDir() {
+            if(bL) dir=Dir.LEFT;
+            if(bU) dir=Dir.UP;
+            if(bR) dir=Dir.RIGHT;
+            if(bD) dir=Dir.DOWN;
         }
 
         @Override
         public void keyReleased(KeyEvent e) {
             //抬起的时候调用
-            System.out.println("key released");
+            int keyCode = e.getKeyCode();
+            switch(keyCode){
+                case KeyEvent.VK_LEFT:
+                    bL=false;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU=false;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD=false;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR=false;
+                    break;
+                default:
+                    break;
+
+            }
+            setMainTankDir();
         }
     }
 }
